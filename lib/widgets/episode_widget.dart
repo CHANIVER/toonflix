8d547a06d00a710005d1d2b23dfb75622bde44d5
CHANIVER <cksdlqj@gmail.com>
2023-12-23 19:42:16 +0900

    DONE

---
+++ lib/widgets/episode_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class Episode extends StatelessWidget {
+  const Episode({
+    super.key,
+    required this.episoed,
+    required this.webtoonId,
+  });
+
+  final WebtoonEpisodeModel episoed;
+  final String webtoonId;
+
+  onButtonTap() async {
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episoed.id}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.green.shade300,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 210,
+                child: Text(episoed.title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15)),
+              ),
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
